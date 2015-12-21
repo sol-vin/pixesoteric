56591f1c131d66927a904c8b360cfd737f46c185
Ian Rash <redcodefinal@gmail.com>
2015-12-21 11:03:00 -0800

    working 0.21
    Added project_euler_5 program

--- simple_run.rb
+++ simple_run.rb
@@ -2,8 +2,8 @@
 
 require './machine'
 
-machine = Machine.new('programs/factorial.bmp', "10")
-200_000.times { machine.run_one_instruction }
+machine = Machine.new('programs/project_euler_5.bmp', "10")
+10_000.times { machine.run_one_instruction }
 
 #machine.run
 puts
